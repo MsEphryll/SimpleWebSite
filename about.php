eebbfd2e76a69a6f5a8614de6537a104a349cb8d
Ephryll <135094289+MsEphryll@users.noreply.github.com>
2024-02-17 12:58:22 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,6 @@
+<?php include 'templates/header.php' ?>
+  <h3>About</h3>
+  <div class="container">
+    <p>This is about the page</p>
+  </div>
+<?php include 'templates/footer.php' ?>
---
+++ start.php
@@ -0,0 +1,6 @@
+<?php include 'templates/header.php' ?>
+  <h3>Home</h3>
+  <div class="container">
+    <p>Welcome to my page!</p>
+  </div>
+<?php include 'templates/footer.php' ?>
