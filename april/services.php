9a3e6d5e953985deb6912f94dcd912075c5a7496
Ephryll <135094289+MsEphryll@users.noreply.github.com>
2024-02-17 12:57:05 +0800

    Delete april directory

--- april/services.php
+++
@@ -1,6 +0,0 @@
-<?php include 'templates/header.php' ?>
-  <h3>Services</h3>
-  <div class="container">
-    <p>Services available</p>
-  </div>  
-<?php include 'templates/footer.php' ?>
--- april/start.php
+++
@@ -1,6 +0,0 @@
-<?php include 'templates/header.php' ?>
-  <h3>Home</h3>
-  <div class="container">
-    <p>Welcome to my page!</p>
-  </div>
-<?php include 'templates/footer.php' ?>
