a5253d82518927fe805dd022d9afe41d0de07408
Ephryll <135094289+MsEphryll@users.noreply.github.com>
2024-02-19 16:30:17 +0800

    updating the look of act2.php

--- act2.php
+++ act2.php
@@ -1,64 +1,61 @@
 <?php include 'templates/header.php' ?>
-<h3>Activity 2</h3>
-  <div class="container">
 
-  <!-- Problem 1 -->
-    <div style="margin-top: 30px;">
-      <h5>Problem 1</h5>
-      <p style="margin-left: 10px;">
-        Write a program to print “Hello World” using echo
-      </p>
-      <h6>Description:</h6>
-      <p style="margin-left: 10px;">
-        Write a program to print “Hello World” using echo only?
-      </p>
-      <h6>Conditions:</h6>
-      <p style="margin-left: 10px;">You can not use any variable</p>
+<div class="card text-white bg-primary mb-3" style="max-width: 500rem;">
+  <div class="card-header">Problem 1</div>
+  <div class="card-body">
+    <!-- Instructions -->
+    <p class="card-text">Write a program to print “Hello World” using echo</p>
+    <h6>Description:</h6>
+    <p style="margin-left: 10px;">
+      Write a program to print “Hello World” using echo only?
+    </p>
+    <h6>Conditions:</h6>
+    <p style="margin-left: 10px;">You can not use any variable</p>
 
-      <h5>Solution:</h5>
-      <p style="margin-left: 10px;">echo "Hello World";</p>
+    <!-- Solution -->
+    <h5>Solution:</h5>
+    <p style="margin-left: 10px;">echo "Hello World";</p>
 
-      <h5>Output:</h5>
-      <p style="margin-left: 10px;">
-        <?php echo "Hello World"; ?>
-      </p>
-    </div>
+    <!-- Output -->
+    <h5>Output:</h5>
+    <p style="margin-left: 10px;">
+      <?php echo "Hello World"; ?>
+    </p>
+  </div>
+</div>
+<div class="card text-white bg-secondary mb-3" style="max-width: 500rem;">
+  <div class="card-header">Problem 2</div>
+  <div class="card-body">
+    <p class="card-text">Write a program to print “Hello PHP” using variable</p>
 
-    <!-- Problem 2 -->
-    <div style="margin-top: 30px;">
-      <h5>Problem 2</h5>
-      <p style="margin-left: 10px;">
-        Write a program to print “Hello PHP” using variable
-      </p>
-      <h6>Description:</h6>
-      <p style="margin-left: 10px;">
-        Write a program to print “Hello PHP” using php variable?
-      </p>
-      <h6>Conditions:</h6>
-      <p style="margin-left: 10px;">
-        You can not use text directly in echo but can use variable.
-      </p>
+    <h6>Description:</h6>
+    <p style="margin-left: 10px;">
+      Write a program to print “Hello PHP” using php variable?
+    </p>
+    <h6>Conditions:</h6>
+    <p style="margin-left: 10px;">
+      You can not use text directly in echo but can use variable.
+    </p>
 
-      <h5>Solution:</h5>
-      <p style="margin-left: 10px;"> $hello = "Hello PHP"; <br>
-          echo "$hello";
-      </p>
+    <h5>Solution:</h5>
+    <p style="margin-left: 10px;"> $hello = "Hello PHP"; <br>
+        echo "$hello";
+    </p>
 
-      <h5>Output:</h5>
-      <p style="margin-left: 10px;">
-        <?php $hello = "Hello PHP";
-          echo "$hello" 
-        ?>
-      </p>
-    </div>
+    <h5>Output:</h5>
+    <p style="margin-left: 10px;">
+      <?php $hello = "Hello PHP";
+        echo "$hello" 
+      ?>
+    </p>
+  </div>
+</div>
+<div class="card text-white bg-success mb-3" style="max-width: 500rem;">
+  <div class="card-header">Problem 3</div>
+  <div class="card-body">
+    <p class="card-text">Write a program to print a string using echo + variable.</p>
 
-    <!-- Problem 3 -->
-    <div style="margin-top: 30px;">
-      <h5>Problem 3</h5>
-      <p style="margin-left: 10px;">
-        Write a program to print a string using echo + variable.
-      </p>
-      <h6>Description:</h6>
+    <h6>Description:</h6>
       <p style="margin-left: 10px;">
         Write a program to print “Welcome to the PHP World” using some part of the text in variable & some part directly in echo.
       </p>
@@ -80,6 +77,7 @@
         echo "Welcome to $a";
         ?>
       </p>
-    </div>
   </div>
+</div>
+
 <?php include 'templates/footer.php' ?>
