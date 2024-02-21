e263aaaf95f7a7ddb0fc2a96a10f663ff51258d0
Ephryll <135094289+MsEphryll@users.noreply.github.com>
2024-02-21 22:40:42 +0800

    Update header.php
    removed the unnecessary lines of codes in head.

--- templates/header.php
+++ templates/header.php
@@ -5,9 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="anothercss.css">
-    <!-- <link rel="stylesheet" href="assets/css/other-styles.css"> -->
-    <!-- how to implement css in php (start.php) -->
 </head>
 <body>
 
