8a8cc8614b3a6d9128dc3bb14510c68c8dd0e40f
Ephryll <135094289+MsEphryll@users.noreply.github.com>
2024-03-02 18:28:45 +0800

    Update about.php
    updated the about page

--- about.php
+++ about.php
@@ -1,6 +1,13 @@
 <?php include 'templates/header.php' ?>
-  <h3>About</h3>
-  <div class="container">
-    <p>This is about the page</p>
+  <!-- about -->
+<div class="card btn-primary mb-3" id="actTitle";>
+  <div class="card-body">
+    <h2 class="act">About The Page</h2>
   </div>
+</div>
+<figure>
+  <figcaption class="blockquote-footer">
+  This section is yet to be filled
+  </figcaption>
+</figure>
 <?php include 'templates/footer.php' ?>
