e66c8da5e605213f1c907c5483fb55538c6c74e7
Ephryll <135094289+MsEphryll@users.noreply.github.com>
2024-03-02 18:49:51 +0800

    Update start.php
    updated the home page

--- start.php
+++ start.php
@@ -1,6 +1,13 @@
 <?php include 'templates/header.php' ?>
-  <h3>Home</h3>
-  <div class="container">
-    <p>Welcome to my page!</p>
+<div class="container">
+  <img src="images/logo.png" style="height=auto; max-width: 25%; display: inline-block; vertical-align: middle; padding: 10px;">
+  <span style="color: #0A1172;
+            font-family: initial; font-size: 35px;padding: 10px; display:inline-block; vertical-align: middle;">WELCOME TO MY PAGE!!!</span>
+</div>
+<div  style="padding: 10px; max-width: 200px; margin-left: 40px; cursor:default;">
+  <h5>April Rose G. Bitang</h5>
+  <div style="text-align: center; margin-left: -43px;"> 
+    <h6>BSIT-student</h6>
+    <h6 style="font-size: 13px;">Central Mindanao University</h6>
   </div>
 <?php include 'templates/footer.php' ?>
